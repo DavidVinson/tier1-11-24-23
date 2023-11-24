3c62a7e78d8c00e1b11673a2491b7f41551c38f9
David Vinson <davidvinson018@gmail.com>
2023-11-24 12:47:53 -0600

    add prettierrc file format

--- practice.js
+++ practice.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+console.log('my statement');
+
+const b = false;
+const myName = 'Dave';
+const MY_STRING = `this is ${myName}'s string`;
+// if (b) {
+//   console.log(MY_STRING);
+// } else {
+//   console.log('this is not my string');
+// }
+
+const myString = b ? MY_STRING : 'this is not my string';
+console.log(myString);
