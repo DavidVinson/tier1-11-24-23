8685d489134c2c111f01d9a608185195847f33d8
David Vinson <davidvinson018@gmail.com>
2023-11-24 13:21:12 -0600

    some examples

--- practice.js
+++ practice.js
@@ -3,6 +3,7 @@ console.log('my statement');
 const b = false;
 const myName = 'Dave';
 const MY_STRING = `this is ${myName}'s string`;
+const myArray = [1, 3, 4, 5];
 // if (b) {
 //   console.log(MY_STRING);
 // } else {
@@ -11,3 +12,18 @@ const MY_STRING = `this is ${myName}'s string`;
 
 const myString = b ? MY_STRING : 'this is not my string';
 console.log(myString);
+
+const objLit = {foo: 'bar'};
+
+// function declarations are hoisted, and can be used anywhere in the file
+myOtherFun();
+
+// function expressions cannot be used before they are created
+myFun();
+const myFun = () => console.log('myfun!');
+
+// this function is created here, but used earlier in the file
+//this is because function declarations are "hoisted".
+function myOtherFun() {
+    console.log('myOther fun!');
+}
